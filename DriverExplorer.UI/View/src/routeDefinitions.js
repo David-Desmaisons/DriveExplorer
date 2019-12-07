3ebe82af691711cab4b582b3c4e1768c0c37357d
Jorge Costa <jorge.costa@trimble.com>
2019-12-07 09:59:04 +0200

    put back sunburst

--- DriverExplorer.UI/View/src/routeDefinitions.js
+++ DriverExplorer.UI/View/src/routeDefinitions.js
@@ -1,6 +1,8 @@
+import main from "./pages/main.vue";
 import about from "./pages/about.vue";
 
 const routeDefinitions = [
+  { name: "main", component: main, menu: { icon: "fa-television" } },
   { name: "about", component: about, menu: { icon: "info" } }
 ];
 
